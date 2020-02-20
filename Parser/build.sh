3fa5f5f5a3535cf70f842156b27d900ec440ef8c
Michael Ness <mness1978@gmail.com>
2020-02-19 22:33:11 -0600

    Parser name change and additions to the BASIC test case.

--- Parser/build.sh
+++ Parser/build.sh
@@ -1,2 +1,2 @@
-peg -o parser.c amos.peg
-gcc -g -o magic magic.c
+leg -o parser.c amos.leg
+gcc -g -o parser parser.c
--- Parser/in.bas
+++ Parser/in.bas
@@ -1,3 +1,5 @@
 A=10+B*3^(5/2)
-Screen Open 0,320,A,16,Lowres
+Screen Open 0,320,A,16,Lowres : B=10-5
+
+C=C+1
 
